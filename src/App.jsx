902cd01f3a11434eeabc6e1cbcbd58fc0d1e80e2
Your Name <your-email@example.com>
2025-09-09 14:41:46 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import "./App.css";
@@ -15,7 +17,7 @@ import DraftsPage from "./pages/proposal/DraftsPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import UserManagement from "./pages/useroles/UserManagement";
-import { initSocket /*    */ } from "./utils/socket";
+import { initSocket } from "./utils/socket";
 import { AllDeals } from "./pages/Deals/allDeals";
 import Pipeline_view from "./pages/Pipeline_View/Pipelien_view";
 import Pipeline_modal_view from "./pages/Pipeline_View/Pipeline_modal_view";
@@ -24,13 +26,11 @@ import CreateDeal from "./pages/Deals/CreateDeal";
 import { NotificationProvider } from "./context/NotificationContext";
 import NotificationsPage from "./pages/notification/NotificationsPage";
 import ViewProposal from "./pages/proposal/ViewProposal";
-// // Tour Components
 import { TourProvider } from "./component/Tour/TourContext";
 import Tour from "./component/Tour/Tour";
-
 import { SocketProvider } from "./context/SocketContext";
 import ViewLead from "./pages/Leads/ViewLead";
-
+import PrivateRoute from "./pages/auth/PrivateRoute";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -44,48 +44,52 @@ function App() {
 
   return (
     <SocketProvider>
-     <TourProvider>
-    <NotificationProvider>
-      <BrowserRouter>
-          <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-all">
+      <TourProvider>
+        <NotificationProvider>
+          <BrowserRouter>
+            <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-all">
               <Tour />
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/layout" element={<Layout />} />
-            <Route element={<Layout isModalOpen={isModalOpen} />}>
-              <Route path="/adminDashboard" element={<AdminDashboard />} />
-              <Route path="/user/roles" element={<UserManagement />} />
-              <Route path="/invoice" element={<InvoiceHead />} />
-              <Route path="/proposal" element={<ProposalHead />} />
-              <Route path="/proposal/sendproposal" element={<SendProposal />} />
-              <Route path="/stage" element={<ProposalBoard />} />
-              <Route path="/calendar" element={<CalendarView />} />
-              <Route path="/list" element={<Activity />} />
-              <Route path="/leads" element={<Leads />} />
-              <Route path="/createleads" element={<CreateLeads />} />
-              <Route path="/deals" element={<AllDeals />} />
-              <Route path="/createDeal" element={<CreateDeal />} />
-              <Route path="/Pipelineview" element={<Pipeline_view />} />
-              <Route
-                path="/Pipelineview/:dealId?"
-                element={<Pipeline_modal_view />}
-              />
-              <Route path="/proposal/drafts" element={<DraftsPage />} />
-              <Route
-                path="/dashboard/notifications"
-                element={<NotificationsPage />}
-              />
-              <Route path="/proposal/view/:id" element={<ViewProposal />} />
-                        <Route path="/leads/view/:id" element={<ViewLead />} />
-          
-            </Route>
-          </Routes>
-          <ToastContainer position="top-right" autoClose={3000} />
-        </div>
-      </BrowserRouter>
-      </NotificationProvider>
+              <Routes>
+                {/* Public Routes */}
+                <Route path="/" element={<Login />} />
+
+                {/* Protected Routes */}
+                <Route element={<PrivateRoute />}>
+                  <Route element={<Layout isModalOpen={isModalOpen} />}>
+                    <Route path="/adminDashboard" element={<AdminDashboard />} />
+                    <Route path="/user/roles" element={<UserManagement />} />
+                    <Route path="/invoice" element={<InvoiceHead />} />
+                    <Route path="/proposal" element={<ProposalHead />} />
+                    <Route path="/proposal/sendproposal" element={<SendProposal />} />
+                    <Route path="/stage" element={<ProposalBoard />} />
+                    <Route path="/calendar" element={<CalendarView />} />
+                    <Route path="/list" element={<Activity />} />
+                    <Route path="/leads" element={<Leads />} />
+                    <Route path="/createleads" element={<CreateLeads />} />
+                    <Route path="/deals" element={<AllDeals />} />
+                    <Route path="/createDeal" element={<CreateDeal />} />
+                    <Route path="/Pipelineview" element={<Pipeline_view />} />
+                    <Route
+                      path="/Pipelineview/:dealId?"
+                      element={<Pipeline_modal_view />}
+                    />
+                    <Route path="/proposal/drafts" element={<DraftsPage />} />
+                    <Route
+                      path="/dashboard/notifications"
+                      element={<NotificationsPage />}
+                    />
+                    <Route path="/proposal/view/:id" element={<ViewProposal />} />
+                    <Route path="/leads/view/:id" element={<ViewLead />} />
+                  </Route>
+                </Route>
+              </Routes>
+
+              <ToastContainer position="top-right" autoClose={3000} />
+            </div>
+          </BrowserRouter>
+        </NotificationProvider>
       </TourProvider>
-      </SocketProvider>
+    </SocketProvider>
   );
 }
 
